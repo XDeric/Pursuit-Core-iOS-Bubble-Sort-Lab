ae6d367926bf81d0cc9e7b9297bd88f11b56e01d
eeeric3995@yahoo.com <ihxx2s7tbu0>
2019-10-09 16:54:41 -0400

    done question 2

--- Pursuit-Core-iOS-Bubble-Sort-Lab/Pursuit-Core-iOS-Bubble-Sort-Lab/BubbleSortQuestions.swift
+++ Pursuit-Core-iOS-Bubble-Sort-Lab/Pursuit-Core-iOS-Bubble-Sort-Lab/BubbleSortQuestions.swift
@@ -5,6 +5,17 @@ import Foundation
 // Implement bubble sort that accepts a closure about how to sort elements
 
 func bubbleSort<T: Comparable>(arr: [T], by isSorted: (T, T) -> Bool) -> [T] {
+    var arr = arr
+    var madeSwap = false
+    repeat{
+        madeSwap = false
+        for i in 0..<arr.count - 1 {
+            if !isSorted(arr[i], arr[i + 1]) && arr[i] != arr[i+1]{
+                arr.swapAt(i, i+1)
+                madeSwap = true
+            }
+        }
+    } while madeSwap
     return arr
 }
 
@@ -14,7 +25,46 @@ func bubbleSort<T: Comparable>(arr: [T], by isSorted: (T, T) -> Bool) -> [T] {
 // https://www.geeksforgeeks.org/sort-an-array-without-changing-position-of-negative-numbers/
 
 func bubbleSortWithoutMovingNegatives<T: Comparable>(arr: [T], by isSorted: (T, T) -> Bool) -> [T] {
-    return arr
+    var arr = arr
+    var ans = [T]()
+    var nums = [T]()
+    var ansIndex = 0
+    let zero = 0 as! T
+    
+    for (i,j) in arr.enumerated(){
+        if (j)  >= zero{
+            ans.append(j)
+            ans = ans.sorted()
+        }
+//        else if (j as! Int) < 0{
+//            ans.insert(j as! Int, at: i)
+//            print(i)
+//        }
+    }
+    print(ans)
+//    for (i,j) in arr.enumerated() {
+//        var count = 0
+//        if j as! Int  >= 0 {
+//            nums.append(ans)
+//
+//        }
+//        else {
+//            nums.insert(j as! Int, at: i )
+//        }
+//    }
+   
+    
+    for i in arr {
+        if i  >= zero {
+            nums.append(ans[ansIndex])
+            ansIndex += 1
+        }else{
+            nums.append(i)
+        }
+    
+    }
+    
+    return nums
 }
 
 // Question Three
